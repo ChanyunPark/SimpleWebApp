c0ce1d6f27c90af820c8ad8a6eb30ae473ad6ab5
ChanyunPark <59681849+ChanyunPark@users.noreply.github.com>
2020-07-09 13:28:40 +0900

    Update MyAppControllerTest.java

--- src/test/java/MyAppControllerTest.java
+++ src/test/java/MyAppControllerTest.java
@@ -3,15 +3,10 @@ package myapp;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
 class MyAppControllerTest {
 
     @Test
-    @RequestMapping("/")
-    public String index() {
-        return "index.html";
+    public String testFunc() {
+        MyAppController myApp = new MyAppController();
     }
 }
