79a4171c672b4e045b1504d4d025c8f69b695247
ChanyunPark <59681849+ChanyunPark@users.noreply.github.com>
2020-07-09 14:01:44 +0900

    Update MyAppController.java

--- src/main/java/myapp/MyAppController.java
+++ src/main/java/myapp/MyAppController.java
@@ -1,14 +1,18 @@
 package myapp;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+//@Controller
 public class MyAppController {
     
-    @RequestMapping("/")
-    public String index() {
-        return "index.html";
+    //@RequestMapping("/")
+    //public String index() {
+    //    return "index.html";
+    //}
+    
+    public int sum(int num1, int num2) {
+        return num1 + numb2;
     }
     
 }
