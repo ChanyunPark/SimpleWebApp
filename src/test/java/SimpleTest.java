374829b90ae8d8220a32278d15ad21bec1407754
ChanyunPark <59681849+ChanyunPark@users.noreply.github.com>
2020-07-09 12:57:25 +0900

    Create SimpleTest.java

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,13 @@
+package myapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleTest {
+
+    @Test
+    String index() {
+        return "index.html";
+    }
+}
